a89843acde4201dc8c77be32434583c318ad65a2
Maria <maria.petrini.woolley@gmail.com>
2023-06-25 15:43:52 -0400

    commit

--- Python/TicTac.py
+++ Python/TicTac.py
@@ -1,89 +1,190 @@
-import numpy as np
 import random
-from time import sleep
-from numpy import append 
+import tkinter
+from tkinter import *
+from functools import partial
+from tkinter import messagebox
+from copy import deepcopy
 
-def create_board():
-    return(np.array([[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]]))
-def possibilities(board):
-    l = []
+sign = 0
+global board
+board = [[" " for x in range(3)] for y in range(3)]
+def winner(b, 1):
+    return((b[0][0] == 1 and b[0][1] == 1 and b[0][2] == 1) or
+            (b[1][0] == 1 and b[1][1] == 1 and b[1][2] == 1) or
+            (b[2][0] == 1 and b[2][1] == 1 and b[2][2] == 1) or
+            (b[0][0] == 1 and b[1][0] == 1 and b[2][0] == 1) or
+            (b[0][1] == 1 and b[1][1] == 1 and b[2][1] == 1) or
+            (b[0][2] == 1 and b[1][2] == 1 and b[2][2] == 1) or
+            (b[0][0] == 1 and b[1][1] == 1 and b[2][2] == 1) or
+            (b[0][2] == 1 and b[1][1] == 1 and b[2][0] == 1))
+def get_text(i, j, gb, l1, l2):
+    global sign
+    if board[i][j] == ' ':
+        if sign % 2 == 0 :
+            l1.config(state=DISABLED)
+            l2.config(state=ACTIVE)
+            board[i][j] = "X"
+        else:
+            l2.config(state=DISABLED)
+            l1.config(state=ACTIVE)
+            board[i][j] = "O"
+        sign += 1
+        button[i][j].config(text=board[i][j])
+    if winner(board, "X"):
+        gb.destroy()
+        box = messagebox.showinfo("Winner", "Player 1 won the match")
+    elif winner(board, "O"):
+        gb.destroy()
+        box = messagebox.showinfo("Tie Game", "Tie Game")
+
+def isfree(i, j):
+    return board[i][j] == " "
+
+def isfull():
+    flag = True
+    for i in board:
+        if(i.count( ' ') > 0):
+            flag = False
+    return flag
+
+def gameboard_pl(game_board, l1, l2):
+    global button
+    button = []
+    for i in range (3):
+        m = 3+i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                    game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
+def pc():
+    possiblemove = []
     for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0:
-                def append (i='i', x='x', y='y'):
-      return(1)
-def random_place(board, player):
-    selection = possibilities(board)
-    current_loc = random.choice(selection)
-    board[current_loc] = player
-    return(board)
-def row_win(board, player):
-  for x in range(len(board)):
-      win = True
-      for y in range(len(board)):
-          if board[x, y] != player:
-              win = False
-              continue
-      if win == True:
-          return(win)
-  return(win)
+        for j in range(len(board[i])):
+            if board[i][j] == ' ':
+                possiblemove.apend([i, j])
+    move = []
+    if possiblemove ==[]:
+        return
+    else:
+        for let in ['O', 'X']:
+                for i in possiblemove:
+                        boardcopy = deepcopy(board)
+                        boardcopy[i[0]][i[1]] = let
+                        if winner(boardcopy, let):
+                                return i
+        corner = []
+        for i in possiblemove:
+                if i in [[0, 0], [0, 2], [1, 2], [2, 1]]:
+                    corner.append(i)
+        if len(corner) > 0:
+            move = random.randit(0, len(corner)-1)
+            return corner[move]
+        edge = []
+        for i in possiblemove:
+                if i in [[0, 1], [1, 0], [2, 1]]:
+                    edge.append(i)
+        if len(edge) > 0:
+            move = random.randint(0, len(edge)-1)
+            return edge[move]
+def get_text_pc(i, j, gb, l1, l2):
+    global sign
+    if board[i][j] == ' ':
+        if sign % 2 == 0 :
+            l1.config(state=DISABLED)
+            l2.config(state=ACTIVE)
+            board[i][j] = "X"
+        else:
+            button[i][j].config(state=ACTIVE)
+            l2.config(state=DISABLED)
+            l1.config(state=ACTIVE)
+            board[i][j] = "O"
+        sign += 1
+        button[i][j].config(text=board[i][j])
+    x = True
+    if winner(board, "X"):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Winner", "Player won the match")
+    elif winner(board, "O"):
+        gb.destroy()
+        x  = False
+        box = messagebox.showinfo("Winner", "Computer won the match")
+    elif(isfull()):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Tie Game", "Tie Game")
+    if(x):
+        if sign % 2 != 0:
+            move = pc()
+            button[move[0]][move[1]].config(state=DISABLED)
+            get_text_pc(move[0], move[1], gb, l1, l2)
+def gameboard_pc(game_board, l1, l2):
+    global button
+    button = []
+    for i in range(3):
+            m = 3 + i
+            button.append(i)
+            button[i] = []
+            for j in range(3):
+                    n = j
+                    button[i].append(j)
+                    get_t = partial(get_text_pc, i, j, game_board, l1, l2)
+                    button[i][j] = Button(
+                            game_board, bd=5, command=get_t, height=4, width=8)
+                    button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
 
-def col_win(board, player):
-    for x in range(len(board)):
-        win = True
-        for y in range(len(board)):
-            if board[y][x] != player:
-              win = False
-              continue
-        if win == True:
-          return(win)
-    return(win)
-
-def diag_win(board, player):
-    win = True
-    y = 0
-    for x in range(len(board)):
-        if board[x, x] != player:
-            win = False 
-    if win:
-        return win 
-    win = True
-    if win:
-        for x in range(len(board)):
-            y = len(board) - 1 - x
-            if board[x, y] != player:
-                win = False 
-    return win
-
-def evaluate(board):
-    winner = 0 
-    
-    for player in [1, 2]: 
-        if (row_win(board, player) or 
-                col_win(board, player) or 
-                diag_win(board, player)):
-            winner = player
-    if np.all(board != 0) and winner ==0:
-        winner = -1 
-    return winner 
+def withpc(game_board):
+    game_board.destroy()
+    game_board = Tk()
+    game_board.title("Tic Tac Toe")
+    l1 = Button(game_board, text="Player : X", width = 10)
+    l2 = Button(game_board, text="Computer : O", width=10, state=DISABLED)
+    l2.grid(row=2, column=1)
+    gameboard_pc(game_board, l1, l2)
   
-def play_game():
-    board, winner, counter = create_board(), 0, 1
-    print(board)
-    sleep(2)
-    
-    while winner == 0:
-        for player in [1, 2]:
-            board = random_place(board, player)
-            print("Board after " + str(counter) + "move")
-            print(board)
-            sleep(2)
-            counter += 1
-            winner = evaluate(board)
-            if winner != 0:
-                break
-    return(winner)
-  
-print("Winner is: " + str(play_game()))
+def withplayer(game_board):
+    game_board.destroy()
+    game_board = Tk ()
+    game_board.title("Tic Tac Toe")
+    l1 = Button(game_board, text ="Player 1 : X", width =10)
+    l1.grid(row=1, column=1)
+    l2 = Button(game_board, text="Player 2: O", width = 10, state=DISABLED)
+    l2.grid(row=2, columns =1)
+    game_board_p1(game_board, l1, l2)
+
+def play():
+    menu = Tk()
+    menu.gemoetry("250X250")
+    menu.title("Tic Tac Toe")
+    wpc = partial(withpc, menu)
+    wp1 = partial(withplayer, menu)
+    head = Button(menu, text="---Welcome to tic-tac-toe---",
+                    activeforeground='saddlebrown',
+                    activebackground="azure", bg ="saddlebrown",
+                    fg="azure", width = 500, font='arial', bd=5)
+    B1 = Button(menu, text="Single Player", command=wpc,
+                    activeforeground='saddlebrown',
+                    activebackground="azure", bg ="saddlebrown",
+                    fg="azure", width = 500, font='arial', bd=5)
+    B2 = Button(menu, text="Multi Player", command=wp1, activeforeground='saddlebrown',
+                    activebackground="azure", bg ="saddlebrown",
+                    fg="azure", width = 500, font='arial', bd=5)
+    B3 = Button(menu, text="Exit", command=menu.quit, activeforeground='saddlebrown',
+                    activebackground="azure", bg ="saddlebrown",
+                    fg="azure", width = 500, font='arial', bd=5)
+    head.pack(side='top')
+    B1.pack(side='top')
+    B2.pack(side='top')
+    B3.pack(side='top')
+    menu.mainloop()
+
+if __name__ == '__main__':
+     play()
+       
